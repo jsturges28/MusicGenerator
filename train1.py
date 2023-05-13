c45fdb01292d3bc8ad6a817199e9cebe967e7acf
jsturges28 <49821904+jsturges28@users.noreply.github.com>
2023-05-12 20:22:02 -0500

    changes

--- train1.py
+++ train1.py
@@ -3,10 +3,14 @@ from tensorflow import keras
 from keras.datasets import mnist
 from autoencoder import Autoencoder
 from vae import VAE
+import os
+import numpy as np
 
-LEARNING_RATE = 0.0001
-BATCH_SIZE = 32
-EPOCHS = 20
+LEARNING_RATE = 0.0005
+BATCH_SIZE = 16
+EPOCHS = 150
+
+SPECTROGRAMS_PATH = os.path.abspath("C:/Users/stur8980/Documents/GitHub/MusicGenerator/spectrograms/")
 
 def load_mnist():
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -19,14 +23,27 @@ def load_mnist():
 
     return x_train, y_train, x_test, y_test
 
+def load_fsdd(spectrograms_path):
+    x_train = []
+    for root, _, file_names in os.walk(spectrograms_path):
+        for file_name in file_names:
+            file_path = os.path.join(root, file_name)
+            spectrogram = np.load(file_path) # (n_bins, n_frames)
+            x_train.append(spectrogram)
+
+    x_train = np.array(x_train)
+    x_train = x_train[..., np.newaxis] # -> (3000, 256, 64, 1) (num_samples, n_bins, n_frames, 1)
+    return x_train
+
+
 
 
 def train(x_train, learning_rate, batch_size, epochs):
-    autoencoder = VAE(input_shape=[28,28,1],
-                              conv_filters=[32, 64, 64, 64],
-                              conv_kernels=[3, 3, 3, 3], 
-                              conv_strides=[1, 2, 2, 1],
-                              latent_space_dim=2)
+    autoencoder = VAE(input_shape=[256,64,1],
+                              conv_filters=[512, 256, 128, 64, 32],
+                              conv_kernels=[3, 3, 3, 3, 3], 
+                              conv_strides=[2, 2, 2, 2, (2,1)],
+                              latent_space_dim=128)
     
     autoencoder.summary()
     autoencoder.compile(learning_rate)
@@ -35,10 +52,10 @@ def train(x_train, learning_rate, batch_size, epochs):
     return autoencoder
 
 if __name__ == "__main__":
-    x_train, _, _, _ = load_mnist()
-    autoencoder = train(x_train[:500], LEARNING_RATE, BATCH_SIZE, EPOCHS)
+    x_train = load_fsdd(SPECTROGRAMS_PATH)
+    autoencoder = train(x_train, LEARNING_RATE, BATCH_SIZE, EPOCHS)
 
     autoencoder.save("model")
 
-    autoencoder2 = Autoencoder.load("model")
-    autoencoder2.summary()
+    #autoencoder2 = Autoencoder.load("model")
+    #autoencoder2.summary()
